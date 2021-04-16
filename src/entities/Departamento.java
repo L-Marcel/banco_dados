95857fb78cfc63c68ab810787a8a7d67ed6e0934
LMarcel <lmgh1312@gmail.com>
2021-04-16 17:30:55 -0300

    Criando toString do departamento

--- src/entities/Departamento.java
+++ src/entities/Departamento.java
@@ -6,19 +6,15 @@ import util.*;
 
 public class Departamento {
 
-    int numero = -1;
-    String nome = "";
-    long cpf_gerente = 0;
-    Date data_ini_gerente = Date.valueOf("0001-01-01");
+  int numero = -1;
+  String nome = "";
+  long cpf_gerente = 0;
+  Date data_ini_gerente = Date.valueOf("0001-01-01");
 
-    @Override
-    public String toString() {
-        return "Departamento{" + "numero=" + numero + ", nome=" + nome + ", cpf_gerente=" + cpf_gerente + ", data_ini_gerente=" + data_ini_gerente + '}';
-    }
+  public Departamento() {
+  }
 
-public Departamento() {}
-
-    public Departamento(int numero, String nome,String cpf_gerente, String data_ini_gerente) {
+  public Departamento(int numero, String nome, String cpf_gerente, String data_ini_gerente) {
     this.numero = numero;
     this.nome = nome;
     this.cpf_gerente = Converter.cpfStringToLong(cpf_gerente);
@@ -56,8 +52,21 @@ public Departamento() {}
   public Date getData_ini_gerente() {
     return data_ini_gerente;
   }
+
   public void setData_ini_gente(Date data_ini_gerente) {
     this.data_ini_gerente = data_ini_gerente;
   }
-  
+
+  @Override
+  public String toString() {
+    return Render.renderLine("Departamento") + "\n" + "Número: " + this.numero + "\n" + "Nome: " + this.nome + " "
+        + "\n" + "CPF do gerente: " + this.getCpf_gerente() + "\n" + "Data de inicio do gerente: "
+        + this.getData_ini_gerente() + "\n" + "\n" + Render.renderLine() + "\n";
+  }
+
+  public String toString(String action) {
+    return Render.renderLine("Departamento " + action) + "\n" + "Número: " + this.numero + "\n" + "Nome: " + this.nome
+        + " " + "\n" + "CPF do gerente: " + this.getCpf_gerente() + "\n" + "Data de inicio do gerente: "
+        + this.getData_ini_gerente() + "\n" + "\n" + Render.renderLine() + "\n";
+  }
 }
