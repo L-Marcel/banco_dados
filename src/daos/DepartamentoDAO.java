d02144e051b4f06a9c613231322d760ed90d98b5
Aninha1834 <meloanaclaudia959@gmail.com>
2021-04-17 13:12:56 -0300

    Selecionar departamento

--- src/daos/DepartamentoDAO.java
+++ src/daos/DepartamentoDAO.java
@@ -31,6 +31,71 @@ public class DepartamentoDAO {
           System.out.print(e);
         }
       };
+      public static Table selecionar(Connection con, String atrs[], String where) {
+        String[] defaultAtrs = { "numero", "nome", "cpf_gerente", "data_ini_gerente"};
+    
+        if (atrs.length == 0) {
+          atrs = defaultAtrs;
+        }
+    
+        String sql = "SELECT " + (Converter.arrayToString(atrs, ",", false)) + " FROM Empregado " + where;
+        String[][] rows = new String[0][0];
+        Table table = new Table("Departamentos Selecionados", atrs, rows);
+    
+        try {
+          PreparedStatement statement = con.prepareStatement(sql);
+          ResultSet response = statement.executeQuery();
+    
+          while (response.next()) {
+            String[] row = new String[0];
+            for (int x = 0; x <= defaultAtrs.length - 1; x++) {
+              String value = "";
+              for (int y = 0; y <= atrs.length - 1; y++) {
+                if (defaultAtrs[x].equals(atrs[y])) {
+                  value = Default.defaultValue("null", response.getString(defaultAtrs[x]));
+                  row = Functions.addInArray(row, value);
+                }
+              }
+            }
+            table.addRow(row);
+          }
+    
+          response.close();
+          statement.close();
+    
+          table.renderTable();
+          return table;
+        } catch (SQLException e) {
+          throw new RuntimeException(e);
+        }
+      };
+      public static Departamento selecionar(Connection con, int numero) {
+        String sql = "SELECT * FROM Departamento WHERE numero=?";
+    
+        try {
+          PreparedStatement statement = con.prepareStatement(sql);
+          statement.setInt(1, numero);
+    
+          ResultSet response = statement.executeQuery();
+    
+          Departamento departamento = new Departamento();
+    
+          while (response.next()) {
+            empregado.setNumero(response.getInt("numero"));
+            empregado.setNome(response.getString("nome"));
+            empregado.setCpf_gerente(response.getLong("cpf_gerente"));
+            empregado.setData_ini_gerente(response.getDate("data_ini_gerente"));
+          }
+    
+          response.close();
+          statement.close();
+    
+          return departamento;
+        } catch (SQLException e) {
+          throw new RuntimeException(e);
+        }
+      };
+    };
       public static Departamento remover(Connection con, int numero) {
         String sql = "DELETE FROM Departamento WHERE numero=?";
         Departamento departamento = selecionar(con, numero);
